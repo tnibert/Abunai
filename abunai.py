bdf8b60327651583c431ec7298dd250c5914a1e5
Tim Nibert <docz2a@gmail.com>
2018-07-12 00:49:10 +1000

    well this is wrong, but hey, we're trying

--- abunai.py
+++ abunai.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 #Abunai v2.0
 #an irc translation bot script
-#released by Timothy Nibert, 29 May 2016
+# a note: https://gist.github.com/gregvish/7665915
 #call with python abunai.py server nick channel USER USERLANGUAGE CHANNELLANGUAGE
 #do not include the # sign in the channel arg
 import sys
 import socket
-import string
+import asyncio
 from textblob import TextBlob
 
 #declare variables for where bot goes
@@ -26,6 +26,8 @@ except:
 	print("do not include the # in the channel name")
 	exit()
 
+CONNOPEN=False
+
 #if you want to manually set the info...
 #HOST=""
 #PORT=6667
@@ -35,12 +37,14 @@ except:
 #CHAN="#abu"
 readbuffer=""					#variable to read in what the server sends us
 
-#open connection to irc server
-s=socket.socket( )
-s.connect((HOST, PORT))
-s.send("NICK {}\r\n".format(NICK).encode())
-s.send("USER {} {} bla :{}\r\n".format(IDENT, HOST, REALNAME).encode())
-s.send("JOIN :{}\r\n".format(CHAN).encode())
+def create_conn():
+    #open connection to irc server
+    s=socket.socket( )
+    s.connect((HOST, PORT))
+    s.send("NICK {}\r\n".format(NICK).encode())
+    s.send("USER {} {} bla :{}\r\n".format(IDENT, HOST, REALNAME).encode())
+    s.send("JOIN :{}\r\n".format(CHAN).encode())
+    return s
 
 def mesg(text, to):
 	s.send("PRIVMSG {} :{}\r\n".format(to, text).encode())
@@ -68,6 +72,57 @@ def transmesg(line, sendto, lang):
 	#except:
 	#	print "same language"
 
+async def init_client(loop):
+    if not CONNOPEN:
+        reader, writer = await asyncio.open_connection(HOST, PORT)
+        writer.write("NICK {}\r\n".format(NICK).encode())
+        writer.write("USER {} {} bla :{}\r\n".format(IDENT, HOST, REALNAME).encode())
+        writer.write("JOIN :{}\r\n".format(CHAN).encode())
+        print("open connection\n")
+        CONNOPEN=True
+    else:
+        pass
+
+async def handle_incoming(reader, writer):
+    if not CONNOPEN:
+        reader, writer = await asyncio.open_connection(HOST, PORT)
+        writer.write("NICK {}\r\n".format(NICK).encode())
+        writer.write("USER {} {} bla :{}\r\n".format(IDENT, HOST, REALNAME).encode())
+        writer.write("JOIN :{}\r\n".format(CHAN).encode())
+        print("open connection\n")
+        CONNOPEN=True
+
+    elif CONNOPEN:
+        data = await reader.read(1024)
+        message = data.decode()
+        temp=message.split("\n")     #remove \n from the readbuffer and store in a temp variable
+        readbuffer=temp.pop( )          #restore readbuffer to empty
+        totranslate = ""
+
+        for line in temp:               #parse through every line read from server
+        #turn line into a list
+            line=line.rstrip()
+            line=line.split()
+
+            #set 2
+            if(line[0]=="PING"):            #if irc server sends a ping, pong back at it
+                s.send("PONG {}\r\n".format(line[1]).encode())
+            elif(line[2]==CHAN):    #if a message comes in from the channel
+                print("message sent from " + CHAN)
+                transmesg(line, USER, userlang)
+                #line[0] is user ident
+
+            elif(line[0][1:len(USER)+1] == USER and line[2]==NICK): #if user privmsg us
+                transmesg(line, CHAN, chanlang)
+            print(totranslate)
+
+            #debug output
+                #print "line: %s" % line
+            #print "lend: %d" % len(line)
+            print("temp: %s" % temp)
+            print("readbuffer: %s" % readbuffer)
+
+"""
 while 1:					#loop FOREVER
     #all of the code between set 1 and set 2 is just putting the message received from the server into a nice format for us
     #set 1
@@ -99,4 +154,18 @@ while 1:					#loop FOREVER
         #print "lend: %d" % len(line)
         print("temp: %s" % temp)
         print("readbuffer: %s" % readbuffer)
+"""
 
+if __name__ == '__main__':
+    print("starting up..")
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(init_client)
+    loop.run_until_complete()
+    loop.create_task(handle_incoming)
+    loop.run_until_complete()
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        sys.exit()
