9caf18fc79b4c6e92e6b6138b7f18ab79cdcf5f5
Tim Nibert <docz2a@gmail.com>
2018-07-13 22:06:21 +1000

    jk we use threading and clean up a bit

--- abunai.py
+++ abunai.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python
-#Abunai v2.0
-#an irc translation bot script
-# a note: https://gist.github.com/gregvish/7665915
-#call with python abunai.py server nick channel USER USERLANGUAGE CHANNELLANGUAGE
-#do not include the # sign in the channel arg
+# Abunai v2.1
+# an irc translation bot script
+# call with python abunai.py server nick channel USER USERLANGUAGE CHANNELLANGUAGE
+# do not include the # sign in the channel arg
+# this script won't play nicely with servers that need nickserv as is
 import sys
 import socket
-import asyncio
+from threading import Thread
 from textblob import TextBlob
 
-#declare variables for where bot goes
+# declare variables for where bot goes
 try:
 	HOST=sys.argv[1]
 	PORT=6667
@@ -26,9 +26,7 @@ except:
 	print("do not include the # in the channel name")
 	exit()
 
-CONNOPEN=False
-
-#if you want to manually set the info...
+# if you want to manually set the info...
 #HOST=""
 #PORT=6667
 #NICK="Bot"
@@ -36,9 +34,10 @@ CONNOPEN=False
 #REALNAME="Bot"
 #CHAN="#abu"
 readbuffer=""					#variable to read in what the server sends us
+threads = []
 
 def create_conn():
-    #open connection to irc server
+    # open connection to irc server
     s=socket.socket( )
     s.connect((HOST, PORT))
     s.send("NICK {}\r\n".format(NICK).encode())
@@ -50,122 +49,71 @@ def mesg(text, to):
 	s.send("PRIVMSG {} :{}\r\n".format(to, text).encode())
 
 def transmesg(line, sendto, lang):
-	totrans = ""
-	translation = ""
-	for x in line[3:]:
-		totrans = totrans + (x + " ")
-	totrans = totrans[1:]
-	blob = TextBlob(totrans)
-	try:				#for error if same language
-		translation = blob.translate(to=lang)
-	except:
-		print("same language")
-	if(sendto == USER):
-		print("test")
-		#mesg(line[0], to)
-		print(line[0])
-		print(translation)
-		print(type(translation))
-		translation = line[0] + ": " + str(translation)
-	mesg(str(translation), sendto)
-	print(translation)
-	#except:
-	#	print "same language"
+    """
+    Translate and send message
+    """
+    totrans = ""
+    translation = ""
+    for x in line[3:]:
+        totrans = totrans + (x + " ")
+    totrans = totrans[1:]
+    blob = TextBlob(totrans)
+    try:				# for error if same language
+        translation = blob.translate(to=lang)
+    except Exception as e:
+        print("THREAD An exception of type {0} occurred. Arguments:\n{1!r}".format(type(e).__name__, e.args))
+        return
+    if(sendto == USER):
+        #mesg(line[0], to)
+        translation = line[0] + ": " + str(translation)
+    mesg(str(translation), sendto)
+    print("THREAD Translation: {}".format(translation))
 
-async def init_client(loop):
-    if not CONNOPEN:
-        reader, writer = await asyncio.open_connection(HOST, PORT)
-        writer.write("NICK {}\r\n".format(NICK).encode())
-        writer.write("USER {} {} bla :{}\r\n".format(IDENT, HOST, REALNAME).encode())
-        writer.write("JOIN :{}\r\n".format(CHAN).encode())
-        print("open connection\n")
-        CONNOPEN=True
-    else:
-        pass
+s = create_conn()
 
-async def handle_incoming(reader, writer):
-    if not CONNOPEN:
-        reader, writer = await asyncio.open_connection(HOST, PORT)
-        writer.write("NICK {}\r\n".format(NICK).encode())
-        writer.write("USER {} {} bla :{}\r\n".format(IDENT, HOST, REALNAME).encode())
-        writer.write("JOIN :{}\r\n".format(CHAN).encode())
-        print("open connection\n")
-        CONNOPEN=True
+while True:					# loop FOREVER (exit with ctrl c)
+    # all of the code between set 1 and set 2 is just putting the message received from the server into a nice format for us
+    # set 1
+    readbuffer=readbuffer+s.recv(1024).decode()		# store info sent from the server into
+    print("MAIN received data")
+    temp=readbuffer.split("\n")		# remove \n from the readbuffer and store in a temp variable
+    readbuffer=temp.pop( )			# restore readbuffer to empty
+    #totranslate = ""
 
-    elif CONNOPEN:
-        data = await reader.read(1024)
-        message = data.decode()
-        temp=message.split("\n")     #remove \n from the readbuffer and store in a temp variable
-        readbuffer=temp.pop( )          #restore readbuffer to empty
-        totranslate = ""
-
-        for line in temp:               #parse through every line read from server
-        #turn line into a list
-            line=line.rstrip()
-            line=line.split()
-
-            #set 2
-            if(line[0]=="PING"):            #if irc server sends a ping, pong back at it
-                s.send("PONG {}\r\n".format(line[1]).encode())
-            elif(line[2]==CHAN):    #if a message comes in from the channel
-                print("message sent from " + CHAN)
-                transmesg(line, USER, userlang)
-                #line[0] is user ident
-
-            elif(line[0][1:len(USER)+1] == USER and line[2]==NICK): #if user privmsg us
-                transmesg(line, CHAN, chanlang)
-            print(totranslate)
-
-            #debug output
-                #print "line: %s" % line
-            #print "lend: %d" % len(line)
-            print("temp: %s" % temp)
-            print("readbuffer: %s" % readbuffer)
-
-"""
-while 1:					#loop FOREVER
-    #all of the code between set 1 and set 2 is just putting the message received from the server into a nice format for us
-    #set 1
-    readbuffer=readbuffer+s.recv(1024).decode()		#store info sent from the server into
-    temp=readbuffer.split("\n")		#remove \n from the readbuffer and store in a temp variable
-    readbuffer=temp.pop( )			#restore readbuffer to empty
-    totranslate = ""
-
-    for line in temp:				#parse through every line read from server
-	#turn line into a list
+    for line in temp:				# parse through every line read from server
+	    # turn line into a list
         line=line.rstrip()
         line=line.split()
 
-        #set 2
+        # set 2
         if(line[0]=="PING"):			#if irc server sends a ping, pong back at it
             s.send("PONG {}\r\n".format(line[1]).encode())
+            print("MAIN PONG")
         elif(line[2]==CHAN):	#if a message comes in from the channel
-            print("message sent from " + CHAN)
-            transmesg(line, USER, userlang)
+            print("MAIN message sent from " + CHAN)
+            thread = Thread(target = transmesg, args = (line, USER, userlang))
+            thread.start()
+            threads.append(thread)
     	    #line[0] is user ident
 
         elif(line[0][1:len(USER)+1] == USER and line[2]==NICK): #if user privmsg us
-            transmesg(line, CHAN, chanlang)
-        print(totranslate)
-        #print(gs.translate(totranslate, 'de'))
+            #transmesg(line, CHAN, chanlang)
+            thread = Thread(target = transmesg, args = (line, CHAN, chanlang))
+            thread.start()
+            threads.append(thread)
 
-        #debug output
-            #print "line: %s" % line
-        #print "lend: %d" % len(line)
-        print("temp: %s" % temp)
-        print("readbuffer: %s" % readbuffer)
-"""
+        # clean up thread pool
+        for t in threads:
+            if not t.isAlive():
+                t.handled = True
+            else:
+                t.handled = False
 
-if __name__ == '__main__':
-    print("starting up..")
+        threads = [t for t in threads if not t.handled]
 
-    loop = asyncio.get_event_loop()
-    loop.create_task(init_client)
-    loop.run_until_complete()
-    loop.create_task(handle_incoming)
-    loop.run_until_complete()
+        print("MAIN Threads: {}".format(len(threads)))
+        #print(totranslate)
 
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        sys.exit()
+        print("MAIN temp: %s" % temp)
+        #print("readbuffer: %s" % readbuffer)
+
